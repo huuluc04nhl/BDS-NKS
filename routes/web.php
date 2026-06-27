8268a58d37e7e99bfe7c3d4cf7744725f2dcee12
huuluc04nhl <huuluc04nhl@gmail.com>
2026-06-27 14:40:30 +0700

    debug: add chat messages to inspect endpoint

--- routes/web.php
+++ routes/web.php
@@ -130,7 +130,8 @@ Route::get('/run-migrations-secure-nks', function () {
             if (request()->query('inspect') === 'true') {
                 return response()->json([
                     'posts' => \App\Models\Post::all(),
-                    'properties' => \App\Models\Property::all()
+                    'properties' => \App\Models\Property::all(),
+                    'chat_messages' => \App\Models\AiChatMessage::orderBy('id', 'desc')->take(20)->get()
                 ]);
             }
 
