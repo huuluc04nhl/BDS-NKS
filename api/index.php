84d2e93997e8d47c24347eb04c8897835dcb3e79
huuluc04nhl <huuluc04nhl@gmail.com>
2026-07-21 06:49:56 +0700

    chore: optimize vercel deployment and cold start performance

--- api/index.php
+++ api/index.php
@@ -12,10 +12,6 @@ if (!is_dir($storagePath)) {
 
 // Override environment variables to force Laravel to use /tmp for compilation
 putenv("VIEW_COMPILED_PATH=/tmp/storage/framework/views");
-putenv("APP_CONFIG_CACHE=/tmp/storage/framework/cache/config.php");
-putenv("APP_ROUTES_CACHE=/tmp/storage/framework/cache/routes.php");
-putenv("APP_SERVICES_CACHE=/tmp/storage/framework/cache/services.php");
-putenv("APP_PACKAGES_CACHE=/tmp/storage/framework/cache/packages.php");
 
 // Forward Vercel Serverless requests to Laravel public bootstrap index
 require __DIR__ . '/../public/index.php';
